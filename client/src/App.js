03f0dc4db51d6dd8a408872cf7aa5b9859bff30f
Iandra Bedin <iandrabedin@gmail.com>
2020-02-18 21:14:12 +0100

    Add React router history

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { Provider } from "react-redux";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { createBrowserHistory } from "history";
 import { store } from "./store";
 import Homepage from "./pages/homepage/homepage";
+
+const history = createBrowserHistory();
 
 const App = () => {
   return (
     <Provider store={store}>
-      <Router>
+      <Router history={history}>
         <Switch>
           <Route exact path="/" component={Homepage}></Route>
           <Route exact path="/homepage" component={Homepage}></Route>
